22853a02257011f98017683330c64305ad2c591c
thmap <dinhthaimap285@gmail.com>
2025-11-13 17:11:13 +0700

    Collision dectections

---
+++ Assets/Scripts/Player/GameInput.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions inputActions;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 input = inputActions.Player.Move.ReadValue<Vector2>();
+
+        input = input.normalized;
+
+        Debug.Log(input);
+        return input;
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,39 +6,61 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking = false;
 
     private void Update()
     {
-        Vector2 input = new Vector2(0, 0);
-
-        // Capture input
-        if (Input.GetKey(KeyCode.W))
-        {
-            input.y += 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            input.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            input.x -= 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            input.x += 1;
-        }
-
-        input = input.normalized;
+        Vector2 input = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(input.x, 0, input.y);
+        
+        bool canMove = !Physics.CapsuleCast(transform.position, 
+                                              transform.position + Vector3.up * 2f, 
+                                              0.7f, 
+                                              moveDir, 
+                                              moveSpeed * Time.deltaTime);
+
+        if(!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, 
+                                              transform.position + Vector3.up * 2f, 
+                                              0.7f, 
+                                              moveDirX, 
+                                              moveSpeed * Time.deltaTime);
+
+            if(canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, 
+                                                  transform.position + Vector3.up * 2f, 
+                                                  0.7f, 
+                                                  moveDirZ, 
+                                                  moveSpeed * Time.deltaTime);
+
+                if(canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    moveDir = Vector3.zero;
+                }
+            }
+        }
+        
+        if(canMove)
+        {
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
 
         // Update walking state
         isWalking = moveDir.magnitude > 0;
-
-        // Move the player
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
 
         // Rotate the player to face the movement direction
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
